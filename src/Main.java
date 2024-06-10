3d42e0344d490d94415f84071126a49cac0180a7
noahh <noah940407@gamil.com>
2024-06-10 17:59:19 +0800

    add bg to start menu

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,11 +18,14 @@ public class Main extends Application{
         ImageView startGame = new ImageView("/resources/images/startGame.png");
         ImageView settings = new ImageView("/resources/images/settings.png");
         ImageView exit = new ImageView("/resources/images/exit.png");
-        center.setAlignment(Pos.CENTER);
         center.getChildren().addAll(startGame, settings, exit);
+        center.setAlignment(Pos.CENTER);
         BorderPane pane_main = new BorderPane();
+        StackPane pane_bg = new StackPane();
         pane_main.setCenter(center);
-        Scene scene = new Scene(pane_main, 1280, 720);
+        pane_bg.getChildren().addAll(new ImageView("/resources/images/start_bg.png"), pane_main);
+        StackPane.setAlignment(center, Pos.CENTER);
+        Scene scene = new Scene(pane_bg, 1280, 720);
         // stage setting
         stage.setTitle("ncuRPG");
         stage.setScene(Loading.scene(scene, 10));
